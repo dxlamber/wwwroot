a8de459a1c384375660e15fc20f62d8de90964e2
Amber <Amber.Dang@morningstar.com>
2016-11-10 16:45:43 +0800

    modify the - , support one argument, two, or more.

--- lisp/Enviroment.js
+++ lisp/Enviroment.js
@@ -3,17 +3,19 @@ GlobalEnv["+"] = function(){
  return [].reduce.call(arguments, function(pv, cv){return pv+cv;}, 0);
 };
 GlobalEnv["*"] = function(){
- debugger;
  return [].reduce.call(arguments, function(pv, cv){return pv*cv;}, 1);
 };
 GlobalEnv["-"] = function(){
- if(arguments.length != 2)
+ if(arguments.length < 1)
  {
-	debugger;
 	return;
  }
-
- return arguments[0] - arguments[1];
+ else if(arguments.length == 1)
+ {
+	 return -arguments[0];
+ }
+ else
+	return [].reduce.call(arguments, function(pv, cv){return pv-cv;});
 };
 GlobalEnv["/"] = function(){
  if(arguments.length != 2)
